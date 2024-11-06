7c40dfe8253937113aebabf6556f9e701d63eb66
Kelvin Martins <kelvin.martins@sempreceub.com>
2024-11-06 16:20:58 -0300

    Alteração de colunas: adição, ajuste de tamanho e remoção de colunas sem função. Mudança na aparência dos botões e adição de novo botão. Ajuste no comando SELECT

--- crud_ta_visitas/crud_ta_visita.py
+++ crud_ta_visitas/crud_ta_visita.py
@@ -5,10 +5,11 @@ from datetime import datetime
 from tkinter import ttk
 from tkinter import messagebox
 
+from IPython.utils.text import columnize
 from jupyter_server.nbconvert.handlers import date_format
 
 from util.db import SQL
-#from c_projeto import IncluirProjeto
+from c_visitas import IncluirVisitas
 
 #from u_projeto import AlterarProjeto
 #from d_projeto import ExcluirProjeto
@@ -23,11 +24,11 @@ class CRUDta_visita(tk.Tk):
         self.title("CRUD - Tabela de Visitas")
 
         # Primeira linha - Título
-        titulo = tk.Label(self, text="Cadastro de Projetos", font='Helvetica 16 bold', fg='blue')
+        titulo = tk.Label(self, text="Cadastro / consulta de visitas", font='Helvetica 16 bold', fg='blue')
         titulo.grid(row=0, column=0, columnspan=3, padx=self.PADX, pady=self.PADY)
 
         # Segunda linha - Parâmetro de consulta
-        lb_nome = tk.Label(self, text="Nome do Projeto", font='Helvetica 12 bold', fg='blue')
+        lb_nome = tk.Label(self, text="Nome do visitante", font='Helvetica 12 bold', fg='blue')
         lb_nome.grid(row=1, column=0, padx=self.PADX, pady=self.PADY)
 
         self.nome_var = tk.StringVar()
@@ -35,7 +36,7 @@ class CRUDta_visita(tk.Tk):
         self.et_nome.grid(row=1, column=1, padx=self.PADX, pady=self.PADY)
 
         self.bt_consultar = tk.Button(self, text="Consultar", command=self.consultar, font='Helvetica 12 bold',
-                                      fg='white', bg='blue')
+                                      fg='white', bg='purple')
         self.bt_consultar.grid(row=1, column=2, padx=self.PADX, pady=self.PADY)
 
         # Treeview para exibir os resultado da consulta no banco de dados
@@ -43,7 +44,7 @@ class CRUDta_visita(tk.Tk):
         style.theme_use('clam')
 
         style.configure("Custom.Treeview", font=("Arial", 12), foreground="blue")
-        self.tre_funcoes = ttk.Treeview(self, columns=("idt_visitantes", "nme_visitante", "rg_visitante", "dta_nascimento", "telefone",
+        self.tre_funcoes = ttk.Treeview(self, columns=("idt_visitantes", "nme_visitante", "rg_visitante", "dta_nascimento",
                                                        "pcd_visitante", "RA_aluno_acompanhante", "dta_visita", "Hora_Ent_Sai"), show="headings", style="Custom.Treeview",height=20)
 
         # Configurar as colunas
@@ -51,7 +52,7 @@ class CRUDta_visita(tk.Tk):
         self.tre_funcoes.heading("nme_visitante", text="Nome do visitante")
         self.tre_funcoes.heading("rg_visitante", text="RG do visitante")
         self.tre_funcoes.heading("dta_nascimento", text="Data de nascimento")
-        self.tre_funcoes.heading("telefone", text="Telefone") #telefone
+        #self.tre_funcoes.heading("telefone", text="Telefone") #telefone
         #self.tre_funcoes.heading("eml_visitante", text="Email do visitante")#email
         self.tre_funcoes.heading("pcd_visitante", text="PCD?") #PCD
         #self.tre_funcoes.heading("cod_aluno_acompanhante", text="Aluno acompanhante")
@@ -59,31 +60,34 @@ class CRUDta_visita(tk.Tk):
         self.tre_funcoes.heading("dta_visita", text="Dia da visita")
         self.tre_funcoes.heading("Hora_Ent_Sai", text="Hora de visita")
         # Ajustar a largura das colunas
-        self.tre_funcoes.column("idt_visitantes", width=10, anchor=tk.CENTER)
-        self.tre_funcoes.column("nme_visitante", width=150, anchor=tk.CENTER)
+        self.tre_funcoes.column("idt_visitantes", width=15, anchor=tk.CENTER)
+        self.tre_funcoes.column("nme_visitante", width=250, anchor=tk.CENTER)
         self.tre_funcoes.column("rg_visitante", width=100, anchor=tk.CENTER)
         self.tre_funcoes.column("dta_nascimento", width=120, anchor=tk.CENTER)
-        self.tre_funcoes.column("telefone", width=100, anchor=tk.CENTER)
+        #self.tre_funcoes.column("telefone", width=100, anchor=tk.CENTER)
         #self.tre_funcoes.column("eml_visitante", width=150)
         self.tre_funcoes.column("pcd_visitante", width=50, anchor=tk.CENTER)
         #self.tre_funcoes.column("cod_aluno_acompanhante", width=100)
         self.tre_funcoes.column("RA_aluno_acompanhante", width=125, anchor=tk.CENTER)
         self.tre_funcoes.column("dta_visita", width=100, anchor=tk.CENTER)
-        self.tre_funcoes.column("Hora_Ent_Sai", width=150, anchor=tk.CENTER)
+        self.tre_funcoes.column("Hora_Ent_Sai", width=170, anchor=tk.CENTER)
         self.tre_funcoes.grid(row=3, column=0, columnspan=15, padx=self.PADX, pady=self.PADY)
 
-        super().geometry("1000x1000") #Tamanho geral da interface
+        super().geometry("1000x650") #Tamanho geral da interface
 
         # Quarta linha com os botões de operações
         self.bt_incluir = tk.Button(self, text="Incluir", command=self.incluir, font='Helvetica 12 bold', fg='white',
-                                    bg='blue')
+                                    bg='purple',width=15)
         self.bt_incluir.grid(row=4, column=0, padx=self.PADX, pady=self.PADY)
         self.bt_alterar = tk.Button(self, text="Alterar", command=self.alterar, font='Helvetica 12 bold', fg='white',
-                                    bg='blue')
+                                    bg='purple',width=15)
         self.bt_alterar.grid(row=4, column=1, padx=self.PADX, pady=self.PADY)
         self.bt_excluir = tk.Button(self, text="Excluir", command=self.excluir, font='Helvetica 12 bold', fg='white',
-                                    bg='blue')
+                                    bg='purple',width=15)
         self.bt_excluir.grid(row=4, column=2, padx=self.PADX, pady=self.PADY)
+        self.bt_insert_visitante = tk.Button(self, text="Excluir", command=self.excluir, font='Helvetica 12 bold', fg='black',
+                                    bg='yellow', width=15)
+        self.bt_insert_visitante.grid(row=4, column=4, padx=self.PADX, pady=self.PADY)
 
         # Criando o objeto que irá acessar o banco de dados
         self.sql = SQL(esquema='bd_gestao_visitantes', pwd='81975907')
@@ -95,7 +99,7 @@ class CRUDta_visita(tk.Tk):
         # Chamar a função da sua classe utilitária para buscar os registros
         cmd = ("select idt_visitantes as IDT, nme_visitante as visitantes, rg_visitante as RG, date_format(dta_nascimento, '%d/%m/%Y') as Data_Nascimento, telefone, pcd_visitante as PCD, "
                "cod_aluno_acompanhante as Aluno, RA_aluno_acompanhante as RA_do_Aluno, date_format(dta_visita, '%d/%m/%Y') as Data_ , concat(hra_entrada_visita, ' - ', "
-               "hra_saida_visita) as Hora_Ent_Sai from tb_visitantes join ta_visitas on idt_visitantes=cod_visitantes join tb_aluno_acompanhante "
+               "hra_saida_visita) as Hora_Ent_Sai from tb_visitantes join ta_visitas on idt_visitantes=cod_visitantes left join tb_aluno_acompanhante "
                "on idt_visitas=cod_aluno_acompanhante where nme_visitante LIKE CONCAT(%s, '%', '%','%')")
 
         #"SELECT * FROM tb_funcao WHERE nme_funcao LIKE CONCAT('%', %s, '%') ORDER BY nme_funcao"
@@ -105,7 +109,7 @@ class CRUDta_visita(tk.Tk):
         self.limpar_tabela()
         for funcao in funcoes:
             self.tre_funcoes.insert("", tk.END, values=(funcao['IDT'], funcao['visitantes'], funcao['RG'],
-                                                        funcao['Data_Nascimento'],funcao['telefone'], funcao['PCD'],
+                                                        funcao['Data_Nascimento'], funcao['PCD'],
                                                         funcao['RA_do_Aluno'], funcao['Data_'], funcao['Hora_Ent_Sai']))
 
     def pegar_idt(self):
@@ -122,7 +126,9 @@ class CRUDta_visita(tk.Tk):
             self.tre_funcoes.delete(funcao)
 
     def incluir(self):
-        pass
+        c = IncluirVisitas(self)
+        self.et_nome.delete(0, tk.END)
+        self.limpar_tabela()
 
     def alterar(self):
         pass
